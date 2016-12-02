9961387580cf4410e70a51d4090f3a9c2f983450
Keisuke Ishikawa <ishikawa-k@mis-kyushu.jp>
2016-12-02 15:46:46 +0900

    プロジェクト ファイルを追加します。

---
+++ PrismDeBadgeOnList/PrismDeBadgeOnList/Views/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace PrismDeBadgeOnList.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
